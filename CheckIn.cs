d3ac4ea0d4065c0ef9db3989f62075b94424b2c3
Andrew Dawson <aldawson1@gmail.com>
2022-12-06 19:19:47 -0500

    Add project files.

---
+++ CheckIn.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    internal class CheckIn
+    {
+        string firstName;
+        string lastName;
+        string address;
+        string phoneNumber;
+        MySqlConnection connection;
+        public CheckIn(MySqlConnection Connection)
+        {
+            connection = Connection;
+            connection.Open();
+        }
+        public void AddUser()
+        {
+
+            Console.Write("Enter First Name:");
+            firstName = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Enter Last Name:");
+            lastName = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Enter Address Name:");
+            address = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Enter Phone Number:");
+            phoneNumber = Console.ReadLine();
+            Console.Clear();
+            string roomNum;           
+        }
+
+        public void ApplyInfo( )
+        {
+            string insertString = string.Empty; 
+
+        
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = "insert into clients (FirstName,LastName,Phone,Address)" +
+                    $"values('{firstName}','{lastName}','{phoneNumber}','{address}');";
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+       
+
+        }
+
+        public void SearchUser()
+        {
+            string str = string.Empty;
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = connection;
+            string searchTerm;
+            Console.Write("Enter Last Name:");
+            searchTerm = Console.ReadLine();
+            cmd.CommandText = $"select * from clients where LastName ='{searchTerm}';";
+            MySqlDataReader reader = cmd.ExecuteReader();
+            cmd.Connection = connection;
+            while(reader.Read() ) 
+            {
+                str += reader.GetString("FirstName");
+                str += "\t";
+                str += reader.GetString("LastName");
+                str += "\t";
+                str += reader.GetString("Phone");
+                str += "\t";
+                str += reader.GetString("Address");
+                str += "\n";
+            }
+            
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using MySql.Data.MySqlClient;
+using static System.Console;
+namespace Hotel
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string connstring = @"server=68.9.31.222;Port=1298;userid=outside;password=1234;database=mydb";
+            MySqlConnection conn;
+            try
+            {
+                conn = new MySqlConnection(connstring);
+                CheckIn checkIn = new CheckIn(conn);
+                while (true)
+                {
+                    Write("New Client? (y/n):");
+                    string nUser = string.Empty;
+                    nUser = ReadLine();
+                    if (nUser == "y" || nUser == "Y")
+                    {
+                        checkIn.AddUser();
+                        checkIn.ApplyInfo();
+                    }
+                    else
+                    {
+                        checkIn.SearchUser();
+                    }
+                }
+             
+
+
+
+            }
+
+            catch (Exception)
+            {
+                WriteLine("DOH!");
+                throw;
+               
+            }
+        
+            
+
+
+        }
+
+
+
+    }
+}
